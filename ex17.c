cd1e70dd597edb1219e8657a7fd74177a7c817b7
Thiago Rodrigo de Oliveira Queiroz <thiago_queiroz94@outlook.com.br>
2016-11-01 14:33:19 -0300

    declaração de struct e main

--- ex17.c
+++ ex17.c
@@ -1,3 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <malloc.h>
+
+typedef struct st_dados
+{
+    int numero;
+    struct st_dados *prox;
+}dados;
+
+int main(void)
+{
+    return EXIT_SUCCESS;
+}
