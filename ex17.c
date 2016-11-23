87617d1e7c011cd13fa9582e6bcff136c5aeb349
Thiago Rodrigo de Oliveira Queiroz <thiago_queiroz94@outlook.com.br>
2016-11-23 11:50:10 -0300

    adicao funcao inserir

--- ex17.c
+++ ex17.c
@@ -10,5 +10,27 @@ typedef struct st_dados
 
 int main(void)
 {
+    do{
+
+    }while()
     return EXIT_SUCCESS;
 }
+
+void inserir(lista **cabeca, int n1)
+{
+    lista *pl=*cabeca;
+    lista *plant=NULL;
+    while(pl!=NULL)
+    {
+        plant=pl;
+        pl=pl->prox;
+    }
+    pl=malloc(sizeof(lista));
+    pl->c=n1;
+    pl->prox=NULL;
+    if(plant!=NULL)
+        plant->prox=pl;
+    else
+        *cabeca=pl;
+}
+
