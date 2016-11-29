3aa73f25b67339bac21a9f1870e968ea27c7ebbf
Thiago Rodrigo de Oliveira Queiroz <thiago_queiroz94@outlook.com.br>
2016-11-29 17:22:33 -0300

    remocao corpo da main - alteraces

--- ex17.c
+++ ex17.c
@@ -14,51 +14,20 @@ int tamanho(lista *cabeca);
 
 int main(void)
 {
-    float num1, num2;
-    char y[MAX];
+    char peg;
+ 
 
-    printf("Calculadora polonesa\n");
-    printf("Se desejar sair da calculadora digite 'y'\n");
-
-    do{
-        printf(": ");
-        fgets(y,MAX,stdin);
-        switch(*y)
+    do
+    {
+        /*calculo();*/
+        printf("Deseja Continuar?\n");
+        printf("y-sim\n");
+        printf("n-não\n");
+        do
         {
-            case '+':
-                num1=remover(&cabeca);
-                num2=remover(&cabeca);
-                printf("%d\n",num1+num2);
-                inserir(&cabeca,num1+num2);
-                break;
-
-            case '-':
-                num1=remover(&cabeca);
-                num2=remover(&cabeca);
-                printf("%d\n",num2-num1);
-                inserir(&cabeca,num2-num1);
-                break;
-
-            case '/':
-                num1=remover(&cabeca);
-                num2=remover(&cabeca);
-                if(num1==0)
-                    printf("Divisao por zero\n");
-                printf("%d\n",num2/num1);
-                inserir(&cabeca,num2/num1);
-                break;
-
-            case '*':
-                num1=remover(&cabeca);
-                num2=remover(&cabeca);
-                printf("%d\n",num1*num2);
-                inserir(&cabeca,num1*num2);
-                break;
-
-            default:
-                inserir(&cabeca,num1);
-        }
-    }while(*y!='y');
+            scanf("%c", &peg);
+        }while(peg=='\n' || (peg!='y' && peg!='n'));
+    }while(peg=='y');
 
     return EXIT_SUCCESS;
 }
